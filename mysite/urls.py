c17e11440524154ec4aea2010cdca682f46a486e
phontom0 <phontom@naver.com>
2016-08-30 14:57:30 +0900

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin2/', include(admin.site.urls)),
     url(r'', include('blog.urls')),
 ]
